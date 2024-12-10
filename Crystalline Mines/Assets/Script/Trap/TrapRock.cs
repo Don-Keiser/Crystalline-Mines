b956d534e8955c4dce9314580816da8ef70e387b
Jsp872 <celian.juan.conti47@gmail.com>
2024-12-10 10:37:07 +0100

    Fix name variable trapRoch
    change traproch to traprock
    Si chameau veut passer par trou d’aiguille, il doit d’abord devenir très mince. De Matheo Haut

--- Crystalline Mines/Assets/Script/Trap/TrapRock.cs
+++ Crystalline Mines/Assets/Script/Trap/TrapRock.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class TrapRock : MonoBehaviour
 {
     private bool _spawned;
-    [SerializeField] private GameObject _trapRoch;
+    [SerializeField] private GameObject _trapRock;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (_spawned == false)
         {
-            Instantiate(_trapRoch, transform.position, transform.rotation);
+            Instantiate(_trapRock, transform.position, transform.rotation);
             StartCoroutine(WaitForSpawn());
         }
     }
