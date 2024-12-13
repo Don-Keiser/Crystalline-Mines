4f6f4ed85adcc51b385f5ee051aaef8190ade124
A-Clivet <a.clivet@lahorde.school>
2024-12-13 15:45:48 +0100

    FeedBack for light interaction
    interactive light switch on/off

--- Crystalline Mines/Assets/Script/Enigma2/ChangeRune.cs
+++ Crystalline Mines/Assets/Script/Enigma2/ChangeRune.cs
@@ -55,8 +55,9 @@ public class ChangeRune : Interactible
         // Deactivate the current GameObject
         if (_displayRune is not null)
         {
-            StartCoroutine(FadeOut(_duration, _displayRune.GetComponent<SpriteRenderer>()));
-            StartCoroutine(LightFadeIn(_duration, _displayRune.GetComponent<Light2D>()));
+            StartCoroutine(FadeOut(_duration/2, _displayRune.GetComponent<SpriteRenderer>()));
+            StartCoroutine(LightFadeIn(_duration/2, _displayRune.GetComponent<Light2D>()));
+            StartCoroutine(CrystalFadeOut(_duration/2, GetComponent<Light2D>()));
         }
 
         // Go to next index
@@ -74,11 +75,12 @@ public class ChangeRune : Interactible
         
         if (_displayRune is not null)
         {
-            TimerManager.StartTimer(_duration, () =>
+            TimerManager.StartTimer(_duration/2, () =>
             {
                 _displayRune.SetActive(true);
-                StartCoroutine(FadeIn(_duration, _displayRune.GetComponent<SpriteRenderer>()));
-                StartCoroutine(LightFadeOut(_duration, _displayRune.GetComponent<Light2D>()));
+                StartCoroutine(FadeIn(_duration/2, _displayRune.GetComponent<SpriteRenderer>()));
+                StartCoroutine(LightFadeOut(_duration/2, _displayRune.GetComponent<Light2D>()));
+                StartCoroutine(CrystalFadeIn(_duration/2, GetComponent<Light2D>()));
             });
         }
         
@@ -96,7 +98,6 @@ public class ChangeRune : Interactible
     
     private IEnumerator FadeOut(float duration, SpriteRenderer sprite)
     {
-        canInteract = false;
         for(float t = duration; t > 0.01f ; t -= Time.deltaTime)
         {
             _alpha = sprite.color;
@@ -105,8 +106,6 @@ public class ChangeRune : Interactible
             yield return null;
         }
         sprite.gameObject.SetActive(false);
-        canInteract = true;
-        OnRuneChanged?.Invoke();
     }
     private IEnumerator FadeIn(float duration, SpriteRenderer sprite)
     {
@@ -120,6 +119,7 @@ public class ChangeRune : Interactible
     }
      private IEnumerator LightFadeIn(float duration, Light2D light2D)
      {
+         canInteract = false;
          light2D.enabled = true;
          for(float t = 0.00f; t < duration ; t += Time.deltaTime)
          {
@@ -131,6 +131,7 @@ public class ChangeRune : Interactible
          
          light2D.pointLightInnerRadius = 1.5f;
          light2D.enabled = false;
+         OnRuneChanged?.Invoke();
      }
     
     private IEnumerator LightFadeOut(float duration, Light2D light2D)
@@ -145,6 +146,32 @@ public class ChangeRune : Interactible
         }
         light2D.pointLightInnerRadius = 0.0f;
         light2D.enabled = false;
+        canInteract = true;
+    }
+    
+    
+    private IEnumerator CrystalFadeOut(float duration, Light2D light2D)
+    {
+        for(float t = duration; t > 0.00f ; t -= Time.deltaTime)
+        {
+            light2D.pointLightInnerRadius = t/duration * 0.5f;
+            light2D.pointLightOuterRadius = t/duration;
+            yield return null;
+        }
+        light2D.pointLightInnerRadius = 0.0f;
+        light2D.pointLightOuterRadius = 0.0f;
+    }
+    
+    private IEnumerator CrystalFadeIn(float duration, Light2D light2D)
+    {
+        for(float t = 0.00f; t < duration ; t += Time.deltaTime)
+        {
+            light2D.pointLightInnerRadius = t/duration * 0.5f;
+            light2D.pointLightOuterRadius = t/duration;
+            yield return null;
+        }
+        light2D.pointLightInnerRadius = 0.5f;
+        light2D.pointLightOuterRadius = 1.0f;
     }
     
     public override void PlayerInteract()
